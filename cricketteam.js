9167eaff6bcb65c0a433bdc3ca174299f8797160
hema <hemukalai02@gmail.com>
2022-03-13 12:54:42 +0530

    js object

---
+++ cricketteam.js
@@ -0,0 +1,65 @@
+//cricket team
+
+const india={
+
+    Teamname:'Indian Cricket Team',
+    Captain:'Rohit Sharma',
+    Representing:'India',
+    Teamcolour:'Blue',
+    Sponsorship:'Nike',
+    player1:{
+        name:'Shikhar Dhawan',
+        role:'Batsman',
+        style:'left handed',
+        age:34
+    },
+    player2:{
+        name:'Virat Kohli',
+        role:'Batsman',
+        style:'left handed',
+        age:34
+    },
+    player3:{
+        name:'Rohit Sharma',
+        role:'Batsman',
+        style:'Right arm offbreak',
+        age:34
+    },
+
+    player4:{
+        name:'Ravichandran Ashwin',
+        role:'Batsman',
+        style:'right handed',
+        age:31
+    },
+    player5:{
+        name:'Deepak Chahar',
+        role:'Batsman',
+        style:'left handed',
+        age:31
+    },
+    player6:{
+        name:'Yuzvendra Chahal',
+        role:'Batsman',
+        style:'left handed',
+        age:31
+    },
+    player7:{
+        name:'Mayank Agarwal',
+        role:'Batsman',
+        style:'right handed',
+        age:31
+    },
+    player8:{
+        name:'Avesh Khan',
+        role:'Batsman',
+        style:'left handed',
+        age:25
+    }
+}
+
+console.log(india.Sponsorship);
+console.log(india.Captain);
+console.log(india.player6);
+console.log(india.player4.role);
+
